fc96089da6fecf2759ea3e60e1236c548355380e
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2022-10-03 07:33:41 +0300

    Visual Selected is working correctly

--- Turn-Based-Strategy-Game/Assets/Scripts/UnitActionSystem.cs
+++ Turn-Based-Strategy-Game/Assets/Scripts/UnitActionSystem.cs
@@ -1,20 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
     [SerializeField] private Unit selectedUnit;
 
     [SerializeField] private LayerMask unitLayerMask;
 
+    public event EventHandler OnSelectedUnitChanged;
 
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("There is moree than one Unit Action System" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetMouseButtonDown(1)) // 0 is left mouse button , 1 is right
+        if (Input.GetMouseButtonDown(1) || Input.GetMouseButton(1)) // 0 is left mouse button , 1 is right
         {
             if (TryHandleUnitSelection()) return;   
             selectedUnit.Move(MouseWorld.GetPosition());
@@ -29,10 +45,21 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
--- Turn-Based-Strategy-Game/Assets/Scripts/UnitSelectedVisual.cs
+++ Turn-Based-Strategy-Game/Assets/Scripts/UnitSelectedVisual.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UnitSelectedVisual : MonoBehaviour
 {
@@ -11,4 +12,30 @@ public class UnitSelectedVisual : MonoBehaviour
     {
         meshRenderer = GetComponent<MeshRenderer>();
     }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+
+        UpdateVisual();
+
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+
 }
